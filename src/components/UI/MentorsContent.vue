7306881447aa2e6ec967a463c75b6a3dcfa9a92a
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-03-25 21:44:02 +0200

    feat: The mentor card component has been added and stylized.

--- src/components/UI/MentorsContent.vue
+++ src/components/UI/MentorsContent.vue
@@ -1,17 +1,188 @@
 <template>
-<div class="mentors-content">
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque delectus dignissimos eos facere, illo itaque libero modi nemo odio totam. At corporis delectus earum illo in perspiciatis quo voluptatem voluptatibus!
-  </p>
-</div>
+  <div class="mentors-content">
+    <div class="mentors-content__top">
+      <div class="mentors-content__title">
+        <h2>{{ mentors_data.title }}</h2>
+        <p>{{ mentors_data.desc }}</p>
+      </div>
+      <div class="mentors-content__btn">
+        <button @click="viewMore">{{ btnText }}</button>
+      </div>
+    </div>
+    <div class="mentors-content__item">
+      <ul>
+        <li v-for="item in MENTORS_ITEM"
+            :key="item.id">
+          <div class="mentors-content__box">
+            <img :src="require('/src/assets/' + item.avatar)"
+                 :alt="item.alt">
+            <h2>{{ item.name }}</h2>
+            <div class="mentors-content__skill">
+              <p>{{ item.skill1 }}</p>
+              <span></span>
+              <p>{{ item.skill2 }}</p>
+            </div>
+            <button @click="viewProfile"> {{ item.btn }}</button>
+          </div>
+        </li>
+      </ul>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters, mapActions} from "vuex";
+
 export default {
   name: "MentorsContent",
+  props: {
+    btnText: {
+      type: String,
+      default: 'View More',
+    },
+    mentors_data: {
+      type: Object,
+      default() {
+        return {};
+      },
+    }
+  },
+  computed: {
+    ...mapGetters(['MENTORS_ITEM']),
+    ...mapActions(['viewProfile']),
+  },
+
+  methods: {
+    viewMore() {
+    }
+  }
 }
+
 </script>
 
 <style lang="scss">
+@use "src/styles/variables" as var;
 
+.mentors-content {
+  &__top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 80px;
+  }
+
+  &__title {
+    width: 500px;
+
+    h2 {
+      font-size: 40px;
+      line-height: 45px;
+      font-weight: var.$font-m;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-size: 16px;
+      line-height: 20px;
+      font-weight: var.$font-r;
+    }
+  }
+
+  &__btn {
+    button {
+      background: var.$default;
+      width: 165px;
+      height: 56px;
+      border-radius: 100px;
+      font-size: 16px;
+      line-height: 20px;
+      color: var.$c100;
+      border: none;
+      @extend %dtrans;
+      &:hover{
+        background: var.$c300;
+        @extend %htrans;
+        color: var.$default;
+      }
+    }
+  }
+
+  &__item {
+    ul {
+      margin-top: 74px;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 26px;
+
+      li {
+
+        width: 282px;
+        background: var.$c400;
+        border-radius: 20px;
+        cursor: pointer;
+        @extend  %dtrans;
+        &:hover{
+          @extend  %border-ef;
+          @extend  %htrans;
+        }
+      }
+    }
+  }
+
+  &__box {
+    padding: 24px;
+    margin: 0 auto;
+    text-align: center;
+
+    h2 {
+      font-size: 20px;
+      line-height: 25px;
+      font-weight: var.$font-sb;
+      text-align: left;
+      margin-top: 15px;
+    }
+
+    button {
+      width: 234px;
+      height: 52px;
+
+      border-radius: 15px;
+      border: none;
+      font-size: 16px;
+      line-height: 20px;
+      font-weight: var.$font-m;
+
+      @extend %btn;
+      @extend %dtrans;
+
+      &:hover {
+        background: var.$c300;
+        @extend %htrans;
+      }
+    }
+  }
+
+  &__skill {
+    display: flex;
+    align-items: center;
+    margin: 9px 0 23px;
+
+    p {
+      font-size: 14px;
+      line-height: 20px;
+      color: var.$c500;
+      font-weight: var.$font-r;
+    }
+
+    span {
+      margin: 0 16px;
+      width: 9px;
+      height: 9px;
+      border-radius: 100%;
+      background: var.$c500;
+
+    }
+  }
+}
 </style>
