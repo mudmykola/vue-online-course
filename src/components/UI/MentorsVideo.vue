9194dc3cac20895c0526b2ac0d6e63d3e850eb9e
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-03-26 13:26:52 +0300

    feat: Created and labeled the MentorsVideo component.

---
+++ src/components/UI/MentorsVideo.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="mentors-video">
+<!-- MentorsVideo Title  -->
+  <div class="mentors-video__title">
+    <h2>{{mentorsVideo_data.title}}</h2>
+    <p>{{mentorsVideo_data.desc}}</p>
+  </div>
+<!-- MentorsVideo List -->
+  <div class="mentors-video__list">
+    <ul>
+      <li>
+        <div class="mentors-video__box">
+          <video src=""></video>
+          <h2></h2>
+          <p></p>
+          <div class="mentors-video__box--like">
+            <span></span>
+            <p></p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MentorsVideo",
+  props:{
+    mentorsVideo_data: {
+      type: Object,
+      default() {
+        return {};
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
