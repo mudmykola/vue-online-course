a24b169946fcd8f354c5d11f56c8ebcd901f139a
sonysmile999@gmail.com <sonysmile999@gmail.com>
2023-03-30 08:02:24 +0300

    feat: We created and stylized a feedback group.

--- src/components/UI/ReviewsContent.vue
+++ src/components/UI/ReviewsContent.vue
@@ -6,37 +6,44 @@
     </div>
     <div class="reviews-cont__list">
       <ul>
-        <li>
+        <li v-for="item in REVIEWS_CONTENT" :key="item.id">
           <div class="reviews-cont__person">
             <div class="reviews-cont__person--ava">
-              <img src="" alt="">
+              <img :src="require('/src/assets/' + item.avatar)"
+                   :alt="item.avaAlt">
               <div class="reviews-cont__person--name">
-                <h2></h2>
-                <p></p>
+                <h2>{{item.name}}</h2>
+                <p>{{item.status}}</p>
               </div>
             </div>
-            <div class="reviews-cont__person--range"></div>
+            <div class="reviews-cont__person--range">
+              <span>{{item.range}}</span>
+            </div>
           </div>
           <div class="reviews-cont__desc">
-            <p></p>
+            <p>{{item.desc}}</p>
           </div>
           <div class="reviews-cont__video">
             <div class="reviews-cont__video--file">
-              <video src=""></video>
+              <video controls ref="videoPlayer"
+                     :src="require('/src/assets/video/' + item.video)"/>
             </div>
             <div class="reviews-cont__video--text">
-              <h2></h2>
-              <p></p>
+              <h2>{{item.title}}</h2>
+              <p>{{item.subname}}</p>
             </div>
           </div>
         </li>
       </ul>
     </div>
-
+<!--<div class="reviews-cont__btn">-->
+<!--  <button @click="createNewReview">Create comment</button>-->
+<!--</div>-->
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
 
 export default {
   name: "ReviewsContent",
@@ -51,7 +58,12 @@ export default {
         return {};
       },
     },
-  }
+  },
+  computed:{
+    ...mapGetters(['REVIEWS_CONTENT']),
+
+  },
+
 }
 </script>
 
@@ -79,6 +91,99 @@ export default {
       width: 344px;
       margin: 0 auto;
       padding-bottom: 52px;
+    }
+  }
+  &__list{
+    cursor: pointer;
+    color: var.$default;
+    ul{
+      display: grid;
+      grid-template-columns: repeat(3,1fr);
+      gap: 24px;
+    }
+    li{
+      width: 384px;
+      background: var.$c100;
+      border-radius: 30px;
+      padding: 24px;
+      @extend %dtrans;
+      &:hover{
+        @extend  %border-ef;
+        @extend %htrans;
+      }
+    }
+  }
+  &__person{
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    &--ava{
+      display: flex;
+      align-items: center;
+    }
+    &--name{
+      margin-left: 16px;
+      h2{
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: var.$font-sb;
+      }
+      p{
+        margin-top: 4px;
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: var.$font-r;
+        color: var.$c700;
+      }
+    }
+    &--range{
+      span{
+        position: relative;
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: var.$font-r;
+        color: var.$c700;
+
+        &:before{
+          position: absolute;
+          content: '';
+          width: 9px;
+          height: 9px;
+          border-radius: 100%;
+          background: var.$c600 ;
+          top: 25%;
+
+          left: -15%;
+        }
+      }
+    }
+
+  }
+  &__desc{
+    margin-top: 15px;
+  }
+  &__video{
+    display: flex;
+    margin-top: 14px;
+    align-items: center;
+    &--file{
+      width: 122px;
+      border-radius: 24px;
+    }
+    &--text{
+      margin-left: 14px;
+      h2{
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: var.$font-sb;
+      }
+      p{
+        margin-top: 11px;
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: var.$font-r;
+        color: var.$c700;
+      }
     }
   }
 
--- src/store/modules/reviews.js
+++ src/store/modules/reviews.js
@@ -1,10 +1,54 @@
 export default {
     state: {
-        reviews: [
+        reviewsContent: [
+            {
+                id: 1,
+                avatar: 'reviews-ava-1.png',
+                avaAlt: 'reviews-ava',
+                name: 'Jane Jesss',
+                status: 'Student',
+                range: 'Watched 73%',
+                desc: '“This is the first time I found a professional online' +
+                    ' class h that they have many instructors to explain the material. Thanks”',
+                video: 'reviews-video-1.mp4',
+                title: 'Designing digital product for enterprise company',
+                subname: 'Name Surname',
 
+            },
+            {
+                id: 2,
+                avatar: 'reviews-ava-1.png',
+                avaAlt: 'reviews-ava',
+                name: 'Jane Jesss',
+                status: 'Student',
+                range: 'Watched 73%',
+                desc: '“This is the first time I found a professional online' +
+                    ' class h that they have many instructors to explain the material. Thanks”',
+                video: 'reviews-video-1.mp4',
+                title: 'Designing digital product for enterprise company',
+                subname: 'Name Surname',
+
+            },
+            {
+                id: 3,
+                avatar: 'reviews-ava-1.png',
+                avaAlt: 'reviews-ava',
+                name: 'Jane Jesss',
+                status: 'Student',
+                range: 'Watched 73%',
+                desc: '“This is the first time I found a professional online' +
+                    ' class h that they have many instructors to explain the material. Thanks”',
+                video: 'reviews-video-1.mp4',
+                title: 'Designing digital product for enterprise company',
+                subname: 'Name Surname',
+
+            },
         ],
     },
     getters: {
-
+        REVIEWS_CONTENT(state) {
+            return state.reviewsContent;
+        }
     },
+
 };
